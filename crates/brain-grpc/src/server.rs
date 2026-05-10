0f7d641ec9f0cca4ececa380a94812f3a2188b48
MrBT-nano <mr.bt1590@gmail.com>
2026-05-10 20:40:56 +0700

    feat: integrate and use vtuber-contracts and vtuber-commons

--- crates/brain-grpc/src/server.rs
+++ crates/brain-grpc/src/server.rs
@@ -1,9 +1,19 @@
+use brain_core::LoreManager;
 use brain_proto::brain::v1::brain_service_server::BrainService;
 use brain_proto::brain::v1::{PushContextRequest, PushContextResponse};
 use tonic::{Request, Response, Status};
 
-#[derive(Default)]
-pub struct MyBrainService {}
+pub struct MyBrainService {
+    pub lore: LoreManager,
+}
+
+impl Default for MyBrainService {
+    fn default() -> Self {
+        Self {
+            lore: LoreManager::new(),
+        }
+    }
+}
 
 #[tonic::async_trait]
 impl BrainService for MyBrainService {
@@ -12,6 +22,7 @@ impl BrainService for MyBrainService {
         request: Request<PushContextRequest>,
     ) -> Result<Response<PushContextResponse>, Status> {
         let r = request.into_inner();
+        tracing::info!("Reasoning with persona lore: {}", self.lore.get_lore());
         tracing::info!("Received context from {}: {}", r.user_id, r.message);
 
         Ok(Response::new(PushContextResponse {
